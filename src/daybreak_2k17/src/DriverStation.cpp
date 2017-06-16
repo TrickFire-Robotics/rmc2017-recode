ac83aac8fb26c6d5f03cd7e5e81b36ba58334f84
Adam Krpan <adam.krpan@outlook.com>
2017-06-15 22:45:36 -0700

    Changed window over to using SFML, our old window framework. Also began publishing messages with drive info.

--- src/daybreak_2k17/src/DriverStation.cpp
+++ src/daybreak_2k17/src/DriverStation.cpp
@@ -1,21 +1,60 @@
 #include <ros/ros.h>
-#include <opencv2/highgui.hpp>
 
-using namespace cv;
+#include <std_msgs/Header.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
+
+#include <daybreak_2k17/DrivePacket.h>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
+using namespace sf;
 
 int main(int argc, char **argv)
 {
-  ROS_INFO("Driver station starting...");
 
   ros::init(argc, argv, "DriverStation");
   ros::NodeHandle nh;
 
-  cv::namedWindow("Hey");
+  ros::Publisher drive_pub = nh.advertise<daybreak_2k17::DrivePacket>("teleop_drive", 100);
 
-  int key;
+  ROS_INFO("Driver station starting...");
+  ROS_DEBUG("Initializing SFML window");
 
-  while ((key = cv::waitKey(0)) != 27) {
-    ROS_INFO("Waiting... key %d", key);
+  RenderWindow window(sf::VideoMode(800, 600), "TrickFire Driver Station");
+
+  while (window.isOpen() && ros::ok()) {
+    Event event;
+
+    while (window.pollEvent(event)) {
+      if (event.type == Event::Closed) {
+        window.close();
+      }
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::W)) {
+      std::cout << "Hey!" << std::endl;
+      ROS_DEBUG("Key W pressed");
+
+      daybreak_2k17::DrivePacket msg;
+      msg.header = std_msgs::Header();
+      msg.l = 1.0f;
+      msg.r = 1.0f;
+      msg.fl = true;
+      msg.fr = true;
+      msg.rl = true;
+      msg.rr = true;
+      drive_pub.publish(msg);
+      ros::spinOnce();
+    }
+
+    window.clear(Color::Black);
+
+    window.display();
   }
-  ROS_INFO("Done!");
+
+  ROS_INFO("DS window closed");
 }
