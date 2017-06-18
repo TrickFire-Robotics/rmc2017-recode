73db300fed91b103317bf6bc2fcf5f9e5aa4edd2
Adam Krpan <adam.krpan@outlook.com>
2017-06-18 10:52:28 -0700

    Added proper motor outputs.
    IMPORTANT NOTE AS OF THIS COMMIT: There is now a CMake variable "GPIO"
    used to enable and disable hardware GPIO outputs. This is set to false
    in the repository currently, due to testing on non-raspi computers.
    Change this to true when compiling on the actual robot to enable
    outputs.

--- src/daybreak_2k17/src/MotorOutput.cpp
+++ src/daybreak_2k17/src/MotorOutput.cpp
@@ -1,13 +1,26 @@
 #include <ros/ros.h>
 
-#include <std_msgs/Header.h>
+#include<std_msgs/Header.h>
 #include<std_msgs/UInt8.h>
 #include<std_msgs/Float32.h>
 
 #include<daybreak_2k17/MotorOutputMsg.h>
 
+#if defined(GPIO)
+#include<wiringPiI2C.h>
+#include<pca9685.h>
+#endif
+
+#if defined(GPIO)
+int pwmFD;
+#endif
+
 void motor_output_callback(const daybreak_2k17::MotorOutputMsg::ConstPtr& msg) {
   ROS_INFO("Received motor output: motor %d to val %f", msg->motorId, msg->val);
+}
+
+unsigned int map_to_motor(const float val) {
+  return (unsigned int)(val * 4095);
 }
 
 int main(int argc, char **argv)
@@ -17,6 +30,21 @@ int main(int argc, char **argv)
 
   ROS_INFO("Motor output module starting...");
   ros::Subscriber sub = nh.subscribe("motor_output", 100, motor_output_callback);
+
+  #if defined(GPIO)
+    ROS_INFO("GPIO is enabled, setting up PCA9685 connection...");
+    pwmFD = pca9685Setup(300, 0x40, 50);
+    if (pwmFD < 0) {
+      ROS_FATAL("Error setting up PCA9685 connection! Node will continue, but no physical outputs will be sent by this node.");
+    } else {
+      pca9685PWMReset(pwmFD);
+      pca9685PWMWrite(pwmFD, 316, 0, map_to_motor(0.5f));
+      ROS_INFO("PCA9685 connection established, all outputs set to neutral (0.0)");
+    }
+
+  #else
+    ROS_INFO("GPIO is not enabled, no physical outputs will be sent by this node.");
+  #endif
 
   ros::spin();
 
