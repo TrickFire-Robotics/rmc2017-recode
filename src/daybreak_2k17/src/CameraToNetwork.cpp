825616061b9d9a244b982234d5da5c2e6e19ffd1
Adam Krpan <adam.krpan@outlook.com>
2017-06-18 18:35:13 -0700

    Added camera framerate restriction and made it also send in scale.

--- src/daybreak_2k17/src/CameraToNetwork.cpp
+++ src/daybreak_2k17/src/CameraToNetwork.cpp
@@ -2,17 +2,26 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <std_msgs/Time.h>
 
 image_transport::Publisher pub;
 
+uint32_t lastCamTime = 0;
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+  if (lastCamTime == 0) {
+    lastCamTime = msg->header.stamp.nsec;
+  }
+
   cv::Mat received = cv_bridge::toCvShare(msg, "bgr8")->image;
   cv::Mat resized;
-  cv::resize(received, resized, cv::Size(512, 512));
+  double scale = 0.25;
+  cv::resize(received, resized, cv::Size((int)(received.cols * scale), (int)(received.rows * scale)));
 
-  if (!resized.empty()) {
+  if ((ros::Time::now().nsec - lastCamTime >= (0.25 * 1000000000L)) && !resized.empty()) {
     sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", resized).toImageMsg();
     pub.publish(msg);
+    lastCamTime = ros::Time::now().nsec;
   }
 }
 
